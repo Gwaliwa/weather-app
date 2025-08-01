cda1af4401cc5b785bf3656460c85e579340d327
gmashaka <gwaliwa2010@gmail.com>
2025-08-01 06:35:44 -0400

    add more urls

--- main.py
+++ main.py
@@ -18,6 +18,21 @@ def about(station, date):
     temperature = df.loc[df["    DATE"] == date]["   TG"].squeeze()/10
     return {"station": station, "date": date, "temperature": temperature
             }
+@app.route("/api/v1/<station>")
+def weather(station):
+    filename = "/Users/gmashaka/Documents/myprojects/weather-app/data_small/TG_STAID"+station.zfill(6)+".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    #station = df.to_dict("records")[0]
+    station = df.to_dict("records")
+    return {"station": station}
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "/Users/gmashaka/Documents/myprojects/weather-app/data_small/TG_STAID"+station.zfill(6)+".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    station = df[df["    DATE"].str.startswith(year)].to_dict("records")
+    return {"station": station}
 
 if __name__ == "__main__":
     app.run(debug=True)
